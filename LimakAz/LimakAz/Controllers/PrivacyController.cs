0ec19e6dce1e12dbf2c3e806e0f2666a3aee86ab
yusifquluzada <yquluzade98@gmail.com>
2021-12-21 00:29:13 +0400

    Privacy Controller Created And Datas Inserted Into Index Page

---
+++ LimakAz/LimakAz/Controllers/PrivacyController.cs
@@ -0,0 +1,27 @@
+﻿using LimakAz.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimakAz.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PrivacyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Privacy> privacies = _context.Privacies.ToList();
+            if (privacies == null) return NotFound();
+
+            return View(privacies);
+        }
+    }
+}
