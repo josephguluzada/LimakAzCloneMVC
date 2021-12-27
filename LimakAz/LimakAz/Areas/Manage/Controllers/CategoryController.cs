254e658e6bf1f5cb8a648bea42dd9f5421dbbf27
yusifquluzada <yquluzade98@gmail.com>
2021-12-27 14:42:31 +0400

    Category Controller created and datas inserted into index

---
+++ LimakAz/LimakAz/Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using LimakAz.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimakAz.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
