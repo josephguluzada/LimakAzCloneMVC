21ac5ccc7f860116c81f573484b51db33f25c9e3
yusifquluzada <yquluzade98@gmail.com>
2022-01-02 23:03:57 +0400

    Member Register done

---
+++ LimakAz/LimakAz/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using LimakAz.Models;
+using LimakAz.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimakAz.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(MemberRegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser member = await _userManager.FindByNameAsync(registerVM.UserName);
+
+            if(member != null)
+            {
+                ModelState.AddModelError("UserName", "İstifadəçi adı hal-hazırda sistemdə mövcuddur");
+                return View();
+            }
+
+            member = await _userManager.FindByEmailAsync(registerVM.Email);
+
+            if(member != null)
+            {
+                ModelState.AddModelError("Email", "Email hal-hazırda sistemdə mövcuddur");
+                return View();
+            }
+
+            member = new AppUser
+            {
+                FullName = registerVM.FullName,
+                UserName = registerVM.UserName,
+                Email = registerVM.Email,
+                PhoneNumber = registerVM.PhoneNumber,
+            };
+
+            var result = await _userManager.CreateAsync(member, registerVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+                return View();
+            }
+
+            await _userManager.AddToRoleAsync(member, "Member");
+            await _signInManager.SignInAsync(member, true);
+
+            return RedirectToAction("index", "home");
+        }
+
+    }
+}
---
+++ LimakAz/LimakAz/ViewModels/MemberRegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimakAz.ViewModels
+{
+    public class MemberRegisterViewModel
+    {
+        [StringLength(maximumLength:50,MinimumLength =6,ErrorMessage ="Ad,Soyad minimum 6 və maksimum 50 uzunluqda ola bilər")]
+        public string FullName { get; set; }
+
+        [StringLength(maximumLength: 30, MinimumLength = 6, ErrorMessage = "İstifadəçi adı minimum 6 və maksimum 30 uzunluqda ola bilər")]
+        public string UserName { get; set; }
+
+        [StringLength(maximumLength: 100, MinimumLength = 8, ErrorMessage = "Email minimum 8 və maksimum 100 uzunluqda ola bilər")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [StringLength(maximumLength: 30, MinimumLength = 8)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(maximumLength: 20, MinimumLength = 6, ErrorMessage = "Şifrə minimum 6 və maksimum 20 uzunluqda ola bilər")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [StringLength(maximumLength: 20, MinimumLength = 6, ErrorMessage = "Şifrə minimum 6 və maksimum 20 uzunluqda ola bilər")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
