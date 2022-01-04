94b615857ff3913f305519eb11fc2773d8bfdc7b
yusifquluzada <yquluzade98@gmail.com>
2022-01-04 13:46:12 +0400

    Member Profile details update done

--- LimakAz/LimakAz/Controllers/AccountController.cs
+++ LimakAz/LimakAz/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using LimakAz.Models;
 using LimakAz.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,11 +14,13 @@ namespace LimakAz.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _context;
 
-        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,AppDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
 
         public IActionResult Register()
@@ -108,6 +111,79 @@ namespace LimakAz.Controllers
             await _signInManager.SignOutAsync();
 
             return RedirectToAction("index", "home");
+        }
+
+        [Authorize(Roles = "Member")]
+        public async Task<IActionResult> Profile()
+        {
+            AppUser member = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            ViewBag.WareHouses = _context.WareHouses.ToList();
+
+            ProfileViewModel profileVM = new ProfileViewModel
+            {
+                FullName = member.FullName,
+                UserName = member.UserName,
+                Email = member.Email,
+                PhoneNumber = member.PhoneNumber,
+                BirthDay = member.BirthDay
+            };
+
+            return View(profileVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Member")]
+        public async Task<IActionResult> Profile(ProfileViewModel profileVM)
+        {
+            ViewBag.WareHouses = _context.WareHouses.ToList();
+
+            if (!ModelState.IsValid) return View();
+
+            AppUser member = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            if (!string.IsNullOrWhiteSpace(profileVM.ConfirmNewPassword) && !string.IsNullOrWhiteSpace(profileVM.NewPassword))
+            {
+                var passwordChangeResult = await _userManager.ChangePasswordAsync(member, profileVM.CurrentPassword, profileVM.NewPassword);
+
+                if (!passwordChangeResult.Succeeded)
+                {
+                    foreach (var item in passwordChangeResult.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+
+                    return View();
+                }
+
+            }
+            if (member.UserName != profileVM.UserName && _userManager.Users.Any(x => x.NormalizedUserName == profileVM.UserName.ToUpper()))
+            {
+                ModelState.AddModelError("UserName", "Bu istifadəçi adı hal-hazırda sistemdə mövcuddur.");
+                return View();
+            }
+
+            member.FullName = profileVM.FullName;
+            member.UserName = profileVM.UserName;
+            member.PhoneNumber = profileVM.PhoneNumber;
+            member.BirthDay = profileVM.BirthDay;
+            member.WareHouseId = profileVM.WareHouse.Id;
+
+            var result = await _userManager.UpdateAsync(member);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+
+                return View();
+            }
+
+            return RedirectToAction("profile", "account");
+
         }
     }
 }
