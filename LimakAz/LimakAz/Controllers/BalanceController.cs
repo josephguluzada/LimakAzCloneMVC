00b2e4a8d3763556ff36ee240e531b0e60878b5c
yusifquluzada <yquluzade98@gmail.com>
2022-01-05 01:29:50 +0400

    Member Balance Increase done

--- LimakAz/LimakAz/Controllers/BalanceController.cs
+++ LimakAz/LimakAz/Controllers/BalanceController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using LimakAz.Models;
+using LimakAz.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,9 +13,56 @@ namespace LimakAz.Controllers
     [Authorize(Roles ="Member")]
     public class BalanceController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public BalanceController(AppDbContext context,UserManager<AppUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            AppUser member = _userManager.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+            if (member == null) return RedirectToAction("index", "error");
+
+            BalanceViewModel balanceVM = new BalanceViewModel
+            {
+                Member = member
+            };
+
+            return View(balanceVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles ="Member")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> IncreaseBalance(BalanceViewModel balanceVM)
+        {
+            if (!ModelState.IsValid) return View();
+            AppUser member = _userManager.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+
+            if (balanceVM.Money != null)
+            {
+                member.Balance = member.Balance + balanceVM.Money;
+            }
+
+            var result = await _userManager.UpdateAsync(member);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+
+                return View();
+            }
+
+
+            return RedirectToAction("index", "balance");
+        }
+
     }
 }
---
+++ LimakAz/LimakAz/ViewModels/BalanceViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LimakAz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimakAz.ViewModels
+{
+    public class BalanceViewModel
+    {
+        public double Money { get; set; }
+        public AppUser Member { get; set; }
+    }
+}
